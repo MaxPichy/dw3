bb1c00a8a58a4c2dd2e3645180b04316c0fff46e
MaxPichy <camilamachadodesouza77@gmail.com>
2026-02-25 22:49:23 -0300

    @MaxPichy

--- aula-01-criando-a-api/routes/gameRoutes.js
+++ aula-01-criando-a-api/routes/gameRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import gameController from '../controllers/gameController.js';
 const gameRoutes = express.Router();
 
 // Na camada de routes são armazenados os ENDPOINTS (URLs) da API
 
 // Endpoint para listar todos os games
-gameRoutes.get('/games', gameController.getAllGames());
+gameRoutes.get('/games', gameController.getAllGames);
 
 export default gameRoutes;
